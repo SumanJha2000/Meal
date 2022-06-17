60d07e94937947db6a0aa4fa634b79d6c3c8bc1f
Suman <86597620+SumanJ2000@users.noreply.github.com>
2022-06-17 15:01:45 +0530

    thirteen

--- mealDetailPage.js
+++ mealDetailPage.js
@@ -10,17 +10,17 @@ window.addEventListener("load", function (e) {
         .then(data => {
             console.log(data.meals);
             var imgContainer = document.querySelector('.meal-details');
-            imgContainer.innerHTML += `<div class="meal-details-left">
-            <h1  class="strMeal" style="font-size:4rem;padding:3rem 0 0 2rem;">${data.meals[0].strMeal}</h1>
-            <h2 style="padding:3rem 1rem 0 2rem;color:blue" >Instructions :</h2>
-            <p style="padding:0rem 1rem 0 2rem;font-size:1.3rem;max-height:28.5%;overflow-y:scroll;">${data.meals[0].strInstructions}</p>
-            <h2 style="padding:3rem 1rem 0 2rem;color:blue" >Category :</h2>
-            <p style="padding:0rem 1rem 0 2rem;font-size:1.6rem;color:white">${data.meals[0].strCategory}</p>
-            <h2 style="padding:3rem 1rem 0 2rem;color:blue" >Country :</h2>
-            <p style="padding:0rem 1rem 0 2rem;font-size:1.6rem;color:white">${data.meals[0].strArea}</p>
-            <h2 style="padding:3rem 1rem 0 2rem;color:blue" >Youtube Tutorial :</h2>
-            <p style="padding:0rem 1rem 0 2rem;color:white;text-decoration:underline;"><a href=${data.meals[0].strYoutube}>${data.meals[0].strYoutube}</a></p>
-           
+            imgContainer.innerHTML += `
+            <div class="meal-details-left">
+            <h1>${data.meals[0].strMeal}</h1>
+            <h2>Instructions :</h2>
+            <p class="instructions">${data.meals[0].strInstructions}</p>
+            <h2>Category :</h2>
+            <p>${data.meals[0].strCategory}</p>
+            <h2>Country :</h2>
+            <p>${data.meals[0].strArea}</p>
+            <h2>Youtube Tutorial :</h2>
+            <p><a style="color:white;text-decoration:underline"href=${data.meals[0].strYoutube}>${data.meals[0].strYoutube}</a></p>
         </div>
         <div class="meal-details-right">
         <img  src=${data.meals[0].strMealThumb}></img>
